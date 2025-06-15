b1cfed65416fbc27a29f440740a764f938ba9fd4
Mohammad <108111892+Mohammad7558@users.noreply.github.com>
2025-06-15 20:28:42 +0600

    added-some-polish

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -84,12 +84,12 @@ const Login = () => {
                   d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
                 />
               </svg>
-              <span className="text-gray-600 font-normal">Enter Your Name</span>
+              <span className="text-gray-600 font-normal">Enter Your Email</span>
             </label>
             <motion.input
               type="text"
               name="email"
-              placeholder="Username"
+              placeholder="Email"
               className="w-full px-5 py-3 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-green-500 transition"
             />
 
