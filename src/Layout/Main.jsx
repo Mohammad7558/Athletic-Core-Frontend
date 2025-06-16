d5c4bee5e4d7b1cecac77039f68ea9b086c4aa2f
Mohammad <108111892+Mohammad7558@users.noreply.github.com>
2025-06-16 23:08:38 +0600

    added-some-changes

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Outlet } from "react-router";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-import CustomCursor from "../components/CustomCursor/CustomCursor";
+// import CustomCursor from "../components/CustomCursor/CustomCursor";
 
 const Main = () => {
   return (
     <div className="bg-gray-100">
-      <div className="hidden lg:block">
+      {/* <div className="hidden lg:block">
         <CustomCursor/>
-      </div>
+      </div> */}
       <Header />
       <Outlet />
       <Footer />
